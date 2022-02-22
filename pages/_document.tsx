1f9db8997402a11b1fe0dc071dbf1307b2b280bf
Daniella Neto <daniella@1pulse.co.za>
2022-02-22 19:30:49 +0200

    Multiple updates - Create a _document.tsx page and move the google font loading here - Add tailsindcss-debug-screens - Add google analytics placeholder - Fix strokeLinecap

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
+class MyDocument extends Document {
+  render() {
+    const isDev = process?.env.NODE_ENV === "development";
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Source+Sans+Pro:wght@200&display=swap"
+            rel="stylesheet"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+            }}
+          />
+        </Head>
+        <body className={isDev ? "debug-screens" : ""}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
