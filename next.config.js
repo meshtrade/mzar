4df5dc40869cdec17c19c38cead16f0b7108adc8
Erick van der Linde <erick@andile.io>
2022-02-24 21:33:20 +0200

    Add the stellar.toml file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/.well-known/stellar.toml',
+        headers: [
+          {
+            key: 'content-type',
+            value: 'text/plain',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
