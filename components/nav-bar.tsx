3c65e37c7c8c56e9beba362c490b6579b0b0f0b9
Daniella Neto <daniella@1pulse.co.za>
2022-02-27 20:32:44 +0200

    navbar background update

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -45,16 +45,16 @@ export const Navbar = () => {
         </button>
         {isMenuOpen && (
           <div className={"absolute z-50 top-0 right-0 w-full"}>
-            <div className={"rounded border mesh-gradient p-5 shadow-sm"}>
+            <div className={"mesh-secondary-gradient shadow-xl p-5 "}>
               <div className={"flex items-center justify-between"}>
                 <div>
                   <button
                     aria-label="Close Menu"
                     title="Close Menu"
-                    className=" -mt-2 -mr-2 rounded p-2 transition duration-200 hover:bg-[#DE1B55]  focus:outline-none"
+                    className=" p-2 transition duration-200 hover:bg-[#DE1B55]  focus:outline-none"
                     onClick={() => setIsMenuOpen(false)}
                   >
-                    <svg className="w-5 text-black" viewBox="0 0 24 24">
+                    <svg className="w-4 text-white" viewBox="0 0 24 24">
                       <path
                         fill="currentColor"
                         d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -69,7 +69,7 @@ export const Navbar = () => {
                   "sm:text-s items-center justify-center text-center md:text-lg"
                 }
               >
-                <ul className="space-y-4 align-middle text-white hover:text-[#DE1B55]">
+                <ul className="space-y-5 align-middle text-white hover:text-[#DE1B55]">
                   <li className="text-white">
                     <Link href="#about">
                       <a
