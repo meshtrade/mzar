4c240c28b4832e55eb16fda2fe528a297d2ca55d
Daniella Neto <daniella@1pulse.co.za>
2022-03-01 09:45:07 +0200

    update found target

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -93,7 +93,6 @@ export const Navbar = (props: NavbarProps) => {
                   <a
                     className={"hover:text-primary"}
                     href="https://app.mesh.trade/sign-up"
-                    target="_blank"
                   >
                     Buy Mzar Now
                   </a>
