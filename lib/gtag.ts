4b0118c389a5351fc87b3d3d67820ca3a22804a6
Erick van der Linde <erick@andile.io>
2022-03-04 13:09:02 +0200

    Enable Google Analytics

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,24 +1,27 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-// export const pageview = (url: string) => {
-//   window.gtag("config", GA_TRACKING_ID, {
-//     page_path: url,
-//   });
-// };
+export const pageview = (url: string) => {
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const sendEvent = ({
-  action,
-  category,
-  label,
-  value,
-}: {
+export interface sendEventRequest {
   action: string;
   category?: string;
   label?: string;
   value?: string;
-}) => {
+}
+
+export const sendEvent = (
+  {
+    action,
+    category,
+    label,
+    value,
+  }: sendEventRequest) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
