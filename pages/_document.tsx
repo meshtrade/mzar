136315e690b5b1f6f12b5ab1e17a316cf66c64bd
Erick van der Linde <erick@andile.io>
2022-06-22 14:09:40 +0200

    Add Google Tag Manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,6 +34,13 @@ class MyDocument extends Document {
         <body className={isDev ? "debug-screens meshScroll overflow-x-hidden" : "meshScroll overflow-x-hidden"}>
           <Main />
           <NextScript />
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52SGPX9" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
         </body>
       </Html>
     );
